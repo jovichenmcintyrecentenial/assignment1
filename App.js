38c88fac094196b465077f564c7123a5330086f2
jovi848 <j.mcintyre@senit.com>
2022-09-17 01:44:51 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,142 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,ScrollView, View,SafeAreaView,TouchableOpacity, TextInput } from 'react-native';
+import React, { useState } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+function SizedBox(prop){
+  return (<View style={{ height: prop.height, width: prop.width }} ></View>);
+}
+
+function LargeText(props){
+  return (<Text style={styles.largeText} >{props.children}</Text>);
+}
+
+function CustomTextInput(props){
+  return(
+  <View style={styles.editTextView}>
+    <TextInput style={styles.editTextInput}
+    clearTextOnFocus={true}>0</TextInput>
+    <Text style={styles.editTextSubTitle}>{props.label}</Text>
+  </View>);
+}
+
+function OptionSelector(props){
+
+  return(
+    <View style={styles.optionSelectorView}>
+      <TouchableOpacity
+          style={[props.state == 0?styles.activeButton:null,styles.optionSelectorButton]}
+          onPress={()=>props.onPress(0)}
+          underlayColor='#fff'>
+          <Text style={styles.optionSelectorText}>Standard</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[props.state == 1?styles.activeButton:styles.inactiveButton,styles.optionSelectorButton]}
+          onPress={()=>props.onPress(1)}
+          underlayColor='#fff'>
+          <Text style={styles.optionSelectorText}>Metric</Text>
+        </TouchableOpacity>
     </View>
   );
 }
 
+export default function App() {
+  const [state ,setButtonState ] = React.useState(0);
+  const [heightStandard ,setHeightStandard ] = React.useState(0);
+  const [heightFeet ,setHeightFeet ] = React.useState(0);
+  const [heightInch ,setHeightInch ] = React.useState(0);
+
+  return (
+    <View >
+      <SafeAreaView style={styles.container} >
+        <Text style={styles.appBarTitle}>BMI Calculator</Text>
+      </SafeAreaView>
+      <ScrollView style={{paddingLeft:20,paddingRight:20}}>
+        <SizedBox height={20} />
+        <OptionSelector state={state} onPress={(i)=>setButtonState(i)}></OptionSelector>
+        <SizedBox height={20} />
+        <LargeText>Your height </LargeText>
+        <SizedBox height={10} />
+        <View style={{flexDirection:'row'}}>
+          <CustomTextInput label="Feet (ft)"></CustomTextInput>
+          <SizedBox width={20} />
+          <CustomTextInput label="Inch (in)"></CustomTextInput>
+        </View>
+
+        <CustomTextInput label="Height (cm)"></CustomTextInput>
+        <SizedBox height={20} />
+
+        <LargeText>Your weight </LargeText>
+        <SizedBox height={10} />
+        <CustomTextInput label="Weight (kg)"></CustomTextInput>
+
+      </ScrollView>
+    </View>
+  );
+}
+const primaryColor = '#325296';
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  editTextSubTitle:{
+    fontSize:25,
+    width:"100%",
+    color:'#939393',
+    textAlign:'center'
   },
+  editTextView:{
+    borderWidth:1,
+    height:104,
+    flex:1,
+    borderRadius:18,
+    borderColor:'#ABB8D4',
+    alignItems:'center',
+    justifyContent:'center'
+    },
+  editTextInput:{
+    fontSize:46,
+    width:"100%",
+    color:'#626262',
+    textAlign:'center'
+  },
+  container: {
+    height:95,
+    backgroundColor: primaryColor,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  optionSelectorView:{
+    padding:5,
+    flexDirection:'row',
+    backgroundColor:'#ABB8D4',
+    borderRadius:1000
+  },
+  largeText:{
+    fontSize:27,
+    color:'#939393'
+
+  },
+  optionSelectorText:{
+    fontSize:27,
+    color:'white'
+
+  },
+  optionSelectorButton:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  activeButton:{
+    height:60,
+    borderRadius:1000,
+    backgroundColor:primaryColor,
+  },
+  inactiveButton:{
+  },
+  appBarTitle:{
+    flex:1,
+    color:'#fff',
+    marginTop:8,
+    fontSize:25,
+    alignItems:'center',
+    justifyContent:'center'
+  }
 });
